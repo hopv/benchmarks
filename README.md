86d03382fab86da0f929e21a5fcc32f36d5c1041
Adrien Champion <adrien.champion@email.com>
2017-08-24 12:47:51 +0900

    File listings and missing benchmarks

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 Functional program verification problems, as caml programs and as Horn clauses.
-
-
-
-# Translation to Horn clauses
-
--- scripts/ml_smt2_diff.sh
+++ scripts/ml_smt2_diff.sh
@@ -3,6 +3,6 @@
 for ml_file in `find caml -iname "*.ml"` ; do
   hc_file=`echo "$ml_file" | sed -e 's:^caml:clauses:' -e 's:ml$:smt2:'`
   if [[ ! -f $hc_file ]] ; then
-    echo "caml file $ml_file has no smt2 counterpart"
+    echo "$ml_file has no smt2 counterpart"
   fi
 done
