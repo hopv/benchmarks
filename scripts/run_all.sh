3fa692089055e183f7c7a799c71fa5e5c383ba6a
AdrienChampion <adrien.champion@email.com>
2017-10-02 18:40:58 +0900

    run all script update

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -8,13 +8,20 @@ else
   sed_cmd="gsed"
 fi
 
+if [ `uname` == "Linux" ] ; then
+  dylib_ext="so"
+else
+  dylib_ext="dylib"
+fi
+
 libs="libs"
-dorder_z3="$libs/dorder_libz3.so"
-fpice_z3="$libs/fpice_libz3.so"
+dorder_z3="$libs/dorder_libz3.$dylib_ext"
+fpice_z3="$libs/fpice_libz3.$dylib_ext"
 
 function cleanup {
   printf "cleaning up..."
   ./scripts/cleanup.sh
+  rm -f "libz3.$dylib_ext"
   echo " success"
   echo
 }
@@ -30,16 +37,19 @@ benchi_cmd="benchi -q -f on run -t 100s --benchs 1 --tools 1"
 
 printf "checking for dorder and fpice z3 libs..."
 if [ ! -f "$dorder_z3" ] ; then
-  echo "\ncould not find \"$dorder_z3\", stopping"
+  echo
+  echo "could not find \"$dorder_z3\", stopping"
   exit 2
 elif [ ! -f "$fpice_z3" ] ; then
-  echo "\ncould not find \"$fpice_z3\", stopping"
+  echo
+  echo "could not find \"$fpice_z3\", stopping"
   exit 2
 fi
 echo " success"
 
 cleanup
 
+cp "$fpice_z3" "libz3.$dylib_ext"
 printf "running fpice and mochi..."
 $benchi -o runs/ml_\<today\> runs/conf/all_ml.conf caml/files
 check_code
@@ -48,6 +58,7 @@ echo
 
 cleanup
 
+cp "$dorder_z3" "libz3.$dylib_ext"
 printf "running dorder..."
 $benchi -o runs/ml_\<today\> runs/conf/dorder.conf caml/files_dorder
 $sed_cmd -i 's:/dorder_lia:/lia:'
